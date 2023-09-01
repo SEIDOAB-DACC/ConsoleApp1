5f5163765a89793715ef489ababf46e9e47789bc
Martin Lenart <martin_lenart@icloud.com>
2023-09-01 10:44:16 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int i = 3;
+string s = "Martin";
+decimal d = 3.5M;
+float f = 3.5F;
+
+Console.WriteLine(i);
+Console.WriteLine(s);
+Console.WriteLine(d);
+Console.WriteLine(f);
+
+string s2 = "3";
+int  i2 = int.Parse(s2);
+
+Console.WriteLine($"Hello {s}! Your decimal number is {d:C5}. Your parsed s2 to i2 is: {i2}");  
+
+Console.WriteLine("Skriv in 3,67");
+
+string tempstring = Console.ReadLine();
+decimal decnr = decimal.Parse(tempstring);
+
+Console.WriteLine(decnr * 10);
